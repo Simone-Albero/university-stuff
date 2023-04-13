1b78132fd3a747e780f7ddff44cc7234a67c689b
Simone-Albero <simone.albero29@gmail.com>
2023-04-13 13:38:50 +0200

    update

--- Basi_di_dati/Esercizio_2.sql
+++
@@ -1,7 +0,0 @@
-drop table if EXISTS Esami;
-CREATE TABLE Esami (
-  Matricola INTEGER PRIMARY KEY,
-  Voto INTEGER,
-  Crediti INTEGER );
-ALTER TABLE Esami Add CONSTRAINT vincolo_cerditi 
-CHECK ((Voto >= 18 and Crediti > 0) OR (Voto <= 18 and Crediti < 0));
--- Progetti/Sudoku_solver/matrix.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MTRIX_H
-#define _MTRIX_H
-
-
-int** file_read(); //restituisce la matrice di partenza del sudoku
-
-
-
-#endif
